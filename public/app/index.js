aa05f66e713037ee26810c333e1bf907833c9943
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-11-18 23:18:28 -0400

    cambios

--- public/app/index.js
+++ public/app/index.js
@@ -1,1 +1,51 @@
-angular.module('estructuras', ['ngMaterial']);
+angular.module('estructuras', ['ngMaterial'])
+    .controller('header', ['$scope', '$mdSidenav', '$timeout', header])
+    .controller('LeftCtrl', ['$scope', '$timeout', '$mdSidenav', '$log' , LeftCtrl]);
+
+function LeftCtrl($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function() {
+        	console.log('clous');
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav('left').close()
+                .then(function() {
+                    $log.debug("close LEFT is done");
+                });
+
+        };
+    }
+
+function header($scope, $mdSidenav, $timeout) {
+    this.toggleLeft = buildDelayedToggler('left');
+
+function buildDelayedToggler(navID) {
+    return debounce(function() {
+        $mdSidenav(navID)
+            .toggle()
+    }, 200);
+}
+
+function debounce(func, wait, context) {
+    var timer;
+
+    return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+            timer = undefined;
+            func.apply(context, args);
+        }, wait || 10);
+    };
+
+    function buildToggler(navID) {
+        return function() {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+                .toggle()
+                .then(function() {
+                    $log.debug("toggle " + navID + " is done");
+                });
+        };
+    }
+   }
+}
